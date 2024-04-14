562ed0cee8871629fd84f438ab098fdd0f3025a2
Vasileios Gkourgkoutas <84097015+vgkourgkoutas@users.noreply.github.com>
2024-04-14 18:15:03 +0300

    Σχεδόν τελειοποιημένη υλοποίηση του Login Form

---
+++ AndroidStudio/Techaholics/app/src/main/java/com/example/techaholics/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.techaholics;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    String user = "shop";
+    String user2 = "client";
+    String user3 = "delivery";
+    String pass = "1234";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            /**
+             * @param v The view that was clicked.
+             */
+            @Override
+            public void onClick(View v) {
+
+                EditText username = (EditText) findViewById(R.id.username);
+                EditText password = (EditText) findViewById(R.id.password);
+
+                // Retrieve entered username and password
+                String Username = username.getText().toString();
+                String Password = password.getText().toString();
+
+                // Implement authentication logic here
+                if ( (Username.equals(user) || Username.equals(user2) || Username.equals(user3)) && Password.equals(pass)) {
+                    // Successful login
+                    Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Failed login
+                    Toast.makeText(MainActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
