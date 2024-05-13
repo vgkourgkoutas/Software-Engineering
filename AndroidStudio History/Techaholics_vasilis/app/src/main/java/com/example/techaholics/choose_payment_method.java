73b0a2e975757870b40982b27b9b23d7e0bb240b
Vasileios Gkourgkoutas <84097015+vgkourgkoutas@users.noreply.github.com>
2024-05-13 22:58:59 +0300

    Add files via upload

--- AndroidStudio History/Techaholics_vasilis/app/src/main/java/com/example/techaholics/choose_payment_method.java
+++ AndroidStudio History/Techaholics_vasilis/app/src/main/java/com/example/techaholics/choose_payment_method.java
@@ -1,41 +1,24 @@
 package com.example.techaholics;
 
-import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-
-public class create_order extends AppCompatActivity {
+public class choose_payment_method extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_create_order);
+        setContentView(R.layout.activity_choose_payment_method);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Button button = (Button) findViewById(R.id.buy);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent startIntent = new Intent(getApplicationContext(), .class);
-                startActivity(startIntent);
-            }
-        });
-
     }
 }
