3ab2aa8680f770a5d180e3594e8bee169f909c11
Vasileios Gkourgkoutas <84097015+vgkourgkoutas@users.noreply.github.com>
2024-05-19 19:51:45 +0300

    Αρχική σύνδεση του Use Case Εφοδιασμός Προϊόντων

--- 3ο Παραδοτέο/Techaholics/app/src/main/java/com/example/techaholics/supplier.java
+++ 3ο Παραδοτέο/Techaholics/app/src/main/java/com/example/techaholics/supplier.java
@@ -11,38 +11,47 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class menu extends AppCompatActivity {
+public class supplier extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_supplier);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button work_cord = (Button) findViewById(R.id.work_cord);
+        Button back_go = (Button) findViewById(R.id.back_go);
 
-        work_cord.setOnClickListener(new View.OnClickListener() {
+        back_go.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent startIntent = new Intent(getApplicationContext(), work_coordination.class);
+                Intent startIntent = new Intent(getApplicationContext(), menu.class);
                 startActivity(startIntent);
             }
         });
 
-        Button supplier_button = (Button) findViewById(R.id.supplier_button);
+        Button apple_supplier = (Button) findViewById(R.id.apple_supplier);
 
-        supplier_button.setOnClickListener(new View.OnClickListener() {
+        apple_supplier.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent startIntent = new Intent(getApplicationContext(), supplier.class);
+                Intent startIntent = new Intent(getApplicationContext(), supplier_products.class);
                 startActivity(startIntent);
             }
         });
 
+        Button sony_supplier = (Button) findViewById(R.id.sony_supplier);
+
+        sony_supplier.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), supplier_products2.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
