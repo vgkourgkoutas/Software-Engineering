17e87d86ff9b59e7c55d4c23cd9f5d9cae010f13
taskont02 <121506867+taskont02@users.noreply.github.com>
2024-05-20 23:31:23 +0300

    Ολοκλήρωση Αγγελίας για εργασία
    Ολοκλήρωση κώδικα αγγελία για εργασία

--- AndroidStudio History/Techaholics_tasos/Techaholics4/app/src/main/java/com/example/techaholics/job_advert.java
+++ AndroidStudio History/Techaholics_tasos/Techaholics4/app/src/main/java/com/example/techaholics/job_advert.java
@@ -1,6 +1,9 @@
 package com.example.techaholics;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,17 @@ public class job_advert extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button sub = (Button) findViewById(R.id.sub);
+
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), message_correctform.class);
+                startActivity(startIntent);
+            }
+        });
+
+
     }
 }
--- AndroidStudio History/Techaholics_tasos/Techaholics4/app/src/main/java/com/example/techaholics/see_forjob.java
+++ AndroidStudio History/Techaholics_tasos/Techaholics4/app/src/main/java/com/example/techaholics/see_forjob.java
@@ -10,22 +10,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class message_correctform extends AppCompatActivity {
+public class see_forjob extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_message_correctform);
+        setContentView(R.layout.activity_see_forjob);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button nextmove = (Button) findViewById(R.id.nextmove);
+        Button backto = (Button) findViewById(R.id.backto);
 
-        nextmove.setOnClickListener(new View.OnClickListener() {
+        backto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent startIntent = new Intent(getApplicationContext(), main_client_page.class);
@@ -33,12 +33,12 @@ public class message_correctform extends AppCompatActivity {
             }
         });
 
-        Button bak = (Button) findViewById(R.id.bak);
+        Button see = (Button) findViewById(R.id.see);
 
-        bak.setOnClickListener(new View.OnClickListener() {
+        see.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent startIntent = new Intent(getApplicationContext(), job_advert.class);
+                Intent startIntent = new Intent(getApplicationContext(), check_form.class);
                 startActivity(startIntent);
             }
         });
