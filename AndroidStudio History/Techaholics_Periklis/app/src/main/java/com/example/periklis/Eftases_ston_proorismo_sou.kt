e920f6c9df8f30dc826804e5242358d37d1964a4
Pkyrkos <162474145+Pkyrkos@users.noreply.github.com>
2024-05-31 22:39:08 +0300

    Τελική ενημέρωση του κώδικα μου
    Εδώ εμπεριέχονται όλες οι οθόνες της "προβολής και επεξεργασίας προιόντων"

---
+++ AndroidStudio History/Techaholics_Periklis/app/src/main/java/com/example/periklis/Eftases_ston_proorismo_sou.kt
@@ -0,0 +1,20 @@
+package com.example.periklis
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Eftases_ston_proorismo_sou : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_eftases_ston_proorismo_sou)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
